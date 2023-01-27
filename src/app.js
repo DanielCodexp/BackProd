aa9fd3b21f044c22c5675d06c4651fd9959e5d57
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2023-01-27 17:59:41 -0600

    cambios

--- src/app.js
+++ src/app.js
@@ -3,16 +3,22 @@ import { pool } from './db.js'
 import {PORT} from './config.js'
 
 const app = express()
-
-app.get('/', async (req, res) => {
+//get productos
+app.get('/products', async (req, res) => {
   const [rows] = await pool.query('SELECT * FROM tbproductos')
   res.json(rows)
 })
-
-app.get('/ping', async (req, res) => {
-  const [result] = await pool.query(`SELECT "hello world" as RESULT`);
-  res.json(result[0])
+// get Reportes
+app.get('/reports',async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM tbrevisar')
+  res.json(rows)
 })
+// get un producto
+app.get('/:id', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM tbproductos where cCodPrd = ?',[id])
+  res.json(rows)
+})
+
 
 app.get('/create', async (req, res) => {
   const result = await pool.query('INSERT INTO users(name) VALUES ("John")')
